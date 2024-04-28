9c6b0fdd6402060e808f1cdccf0e683e11031766
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-28 23:41:46 +0200

    Update contactsSchemas.js

--- schema/contactsSchemas.js
+++ schema/contactsSchemas.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string()
         .min(3)
-        .max(10)
+        .max(20)
         .required(),
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: false } })
