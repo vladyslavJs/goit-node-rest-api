0d84298417041ee74375e619dce67a8b44219a9e
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-01 13:34:54 +0200

    default-values

--- schema/contactsSchemas.js
+++ schema/contactsSchemas.js
@@ -1,27 +1,9 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-    name: Joi.string()
-        .min(3)
-        .max(10)
-        .required(),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: false } })
-        .required(),
-    phone: Joi.string()
-        .pattern(/^[0-9]{10}$/)
-        .required(),
-});
+
+})
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string()
-        .min(6)
-        .max(15)
-        .required(),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: false } })
-        .required(),
-    phone: Joi.string()
-        .pattern(/^[0-9]{10}$/)
-        .required(),
-});
+
+})
