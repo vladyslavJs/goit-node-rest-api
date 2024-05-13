fc1b649286e07bdc6e9379aeff05c08b107397ff
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-13 21:56:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
-app.use("/api/users", usersRouter)
+app.use("/api/users", usersRouter);
+
 
 app.use((req, res, next) => {
   res.status(404).send("Route not found");
