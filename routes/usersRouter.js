e1901a5f42753a0442664490f86364cdcde4e989
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-13 21:57:03 +0200

    Update usersRouter.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { register } from "../controllers/userControllers.js"
+import { register, login } from "../controllers/userControllers.js"
 
 const usersRouter = express.Router();
 const jsonParser = express.json();
 
 usersRouter.post("/register", jsonParser, register);
+usersRouter.post("/login", jsonParser, login);
 
 
 export default usersRouter;
