fb790c701a8011c238d8f791d86ff668ec51f2fe
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-13 14:53:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
-
+import usersRouter from "./routes/usersRouter.js";
 import "./db.js";
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter)
 
 app.use((req, res, next) => {
   res.status(404).send("Route not found");
