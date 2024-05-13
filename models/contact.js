ccb56372dc4795f1b5e909f99544c104f3c7ef7d
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-13 14:53:56 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema(
-    {
+  {
     name: {
       type: String,
       required: [true, 'Set name for contact'],
     },
     email: {
-        type: String,
+      type: String,
     },
     phone: {
-        type: String,
+      type: String,
     },
     favorite: {
       type: Boolean,
       default: false,
     },
-  }, {
+    // owner: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: 'user',
+    // }
+  },
+  {
     versionKey: false,
     timestamps: true,
   }
