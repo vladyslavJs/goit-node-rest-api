510f0c9b7bf979d52bdec442103f29372e3a0da3
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-14 13:25:11 +0200

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,20 +2,16 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
 import User from "../models/user.js"
 import HttpError from "../helpers/HttpError.js";
-
-// const { DB_URI } = process.env;
-
+import e from "express";
 
 export async function register(req, res, next) {
     const { email, password } = req.body;
-
     try {
         const user = await User.findOne({ email });
 
         if (user !== null) {
             throw HttpError(409, "Email in use");
         }
-
         const passwordHash = await bcrypt.hash(password, 10);
         await User.create(
             {
@@ -34,7 +30,6 @@ export async function register(req, res, next) {
 
 export async function login(req, res, next) {
     const { email, password } = req.body;
-
     try {
         const user = await User.findOne({ email });
 
@@ -50,19 +45,35 @@ export async function login(req, res, next) {
             throw HttpError(401, "Email or password is incorrect");
         }
 
+        const JWT_SECRET = process.env.JWT_SECRET;
         const payload = { id: user._id, name: user.name };
 
-        // const token = jwt.sign(
-        //     payload,
-        //     process.env.JWT_SECRET,
-        //     { expiresIn: 60 * 60 },
-        // );
+        const token = jwt.sign(
+            payload,
+            JWT_SECRET,
+            { expiresIn: "10h" },
+        );
 
         await User.findByIdAndUpdate(user._id, { token });
 
         res.status(201).json({ token: token, user: { email, subscription: user.subscription } });
-
     } catch (error) {
         next(error);
     }
 }
+
+export async function logout(req, res, next) {
+    try {
+        const user = await User.findByIdAndUpdate(req.user._id, { token: null });
+
+        if (user === null) {
+            throw HttpError(401, "Not authorized")
+        }
+
+        res.status(204).end();
+    } catch (error) {
+        next(error);
+    }
+}
+
+
