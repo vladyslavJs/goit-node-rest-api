60c0cc72a70f375fd96d9f9af838959928c98c88
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-15 19:24:29 +0200

    Update usersSchemas.js

--- schema/usersSchemas.js
+++ schema/usersSchemas.js
@@ -1,1 +1,14 @@
 import Joi from "joi";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+export const registerUserSchema = Joi.object({
+    name: Joi.string().min(1).trim(),
+    email: Joi.string().pattern(emailRegexp).lowercase().trim().required(),
+    password: Joi.string().min(6).trim().required()
+})
+
+export const loginUserSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).lowercase().trim().required(),
+    password: Joi.string().min(6).trim().required()
+})
