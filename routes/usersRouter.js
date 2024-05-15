8f057c44f78eef19c5bcd4e61a7e12c36cdb4549
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-15 19:24:27 +0200

    Update usersRouter.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,14 +4,15 @@ import express from "express";
 import { register, login, logout, current } from "../controllers/userControllers.js";
 import { autMiddleware } from "../helpers/authMiddleware.js";
 import { validateBody } from "../helpers/validateBody.js";
+import { loginUserSchema, registerUserSchema } from "../schema/usersSchemas.js";
 
 const usersRouter = express.Router();
 const jsonParser = express.json();
 
-usersRouter.post("/register", validateBody, jsonParser, register);
-usersRouter.post("/login", validateBody, jsonParser, login);
+usersRouter.post("/register", validateBody(registerUserSchema), jsonParser, register);
+usersRouter.post("/login", validateBody(loginUserSchema), jsonParser, login);
 usersRouter.post("/logout", autMiddleware, jsonParser, logout);
-usersRouter.post("/current", current);
+usersRouter.post("/current", autMiddleware, current);
 
 
 export default usersRouter;
