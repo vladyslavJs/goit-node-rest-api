9b47c097212ed6802ff96a5c989e59f8b25b102a
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-29 11:45:27 +0200

    update-routers-controllers

---
+++ controllers/authControllers.js
@@ -0,0 +1,126 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import gravatar from "gravatar";
+import HttpError from "../helpers/HttpError.js";
+import User from "../models/user.js"
+import { sendMail } from "../helpers/emailVerify.js";
+import "dotenv/config";
+import { nanoid } from "nanoid";
+import { log } from "console";
+
+export async function register(req, res, next) {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (user !== null) {
+            throw HttpError(409, "Email in use");
+        }
+
+        const avatarURL = gravatar.url(email);
+
+        const passwordHash = await bcrypt.hash(password, 10);
+        const verificationToken = nanoid();
+
+        const newUser = await User.create({
+            email,
+            password: passwordHash,
+            avatarURL,
+        });
+
+        sendMail({
+            to: email,
+            from: process.env.EMAIL_SENDER,
+            subject: "Welcome to our service!",
+            html: `<h2 style="color: #808080">To complete your registration and activate your account, please click on the following link:<a href="http://localhost:3000/api/usersRouter/verify/${verificationToken}">link<a></h2>`,
+            text: `To complete your registration and activate your account, please click on the following link:<a href="http://localhost:3000/api/usersRouter/verify/${verificationToken}"`
+        });
+
+
+        const { subscription } = newUser;
+
+        res.status(201).json({
+            user:
+            {
+                email,
+                subscription,
+                avatarURL,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export async function login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (user === null) {
+            console.log("Email");
+            throw HttpError(401, "Email or password is incorrect");
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password);
+
+        if (comparePassword === false) {
+            console.log("Password");
+            throw HttpError(401, "Email or password is incorrect");
+        }
+
+        const payload =
+        {
+            id: user._id,
+            name: user.name,
+        };
+
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" },
+        );
+
+        await User.findByIdAndUpdate(user._id, { token });
+
+        res.status(201).json({
+            token,
+            user: {
+                email: user.email,
+                subscription: user.subscription,
+                avatarURL: user.avatarURL,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function logout(req, res, next) {
+    try {
+        const user = await User.findByIdAndUpdate(req.user.id, { token: null });
+
+        if (user === null) {
+            throw HttpError(401, "Not authorized")
+        }
+
+        res.status(204).end();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function current(req, res, next) {
+    try {
+        const user = await User.findById(req.user.id)
+
+        if (user === null) {
+            throw HttpError(401, "Not authorized")
+        }
+
+        res.status(200).json({ email: user.email, subscription: user.subscription });
+    }
+    catch (error) {
+        next(error);
+    }
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,20 +1,19 @@
 import express from "express";
 
-import { register, login, logout, current, getAvatar, updateAvatar, verify, resendVerificationEmail } from "../controllers/userControllers.js";
+import {
+    getAvatar,
+    updateAvatar,
+    verify,
+    resendVerificationEmail
+} from "../controllers/userControllers.js";
 
 import { authMiddleware } from "../helpers/authMiddleware.js";
 import { update } from "../helpers/upload.js";
 
 import { validateBody } from "../helpers/validateBody.js";
-import { loginUserSchema, registerUserSchema, verifyUserSchema } from "../schema/usersSchemas.js";
+import { verifyUserSchema } from "../schema/usersSchemas.js";
 
 const usersRouter = express.Router();
-const jsonParser = express.json();
-
-usersRouter.post("/register", validateBody(registerUserSchema), jsonParser, register);
-usersRouter.post("/login", validateBody(loginUserSchema), jsonParser, login);
-usersRouter.post("/logout", authMiddleware, jsonParser, logout);
-usersRouter.get("/current", authMiddleware, current);
 
 usersRouter.get("/avatars", authMiddleware, getAvatar);
 usersRouter.patch("/avatars", authMiddleware, update.single("avatar"), updateAvatar);
