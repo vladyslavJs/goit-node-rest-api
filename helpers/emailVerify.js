2532fd4aa50aeeefd56a2c0f8f46f896fd559d59
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-05-31 11:55:19 +0200

    Update emailVerify.js

--- helpers/emailVerify.js
+++ helpers/emailVerify.js
@@ -10,9 +10,23 @@ const transport = nodemailer.createTransport({
     },
 });
 
-export const sendMail = (message) => {
+export const sendMail = ({ to, verificationToken }) => {
+    const message = {
+        to,
+        from: process.env.EMAIL_SENDER,
+        subject: "Welcome to our service!",
+        html:
+            `<h2 style="color: #000000; font-family: Arial;">To complete your registration and activate your account, <span style="color: #FF0000;">please</span> click on the following link:
+                <a href="${process.env.LOCAL_HOST}/users/verify/${verificationToken}"> link </a>
+            </h2>
+                <p style="font-size: 22px; font-family: Arial; color: #000000; font-weight: 600;text-align: center;">Nice to meet you ✅</p>`,
+        text: `To complete your registration and activate your account, please click on the following link:
+            <a href="${process.env.LOCAL_HOST}/users/verify/${verificationToken}"</a>`
+    };
     return transport.sendMail(message);
 };
+
+
 
 
 
